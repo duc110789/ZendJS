a1357b7b583dc067d5da162cd31d7e0cd1f4eb7a
duc110789 <duc110789@gmail.com>
2018-08-31 08:10:47 +0700

    add file App, Course

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,30 @@ import Course from'./component/Course';
 
 class App extends Component {
     render() {
+        const items = [
+            {
+                name: 'ReactJS',
+                time: '40h',
+                free: false,
+                desc: 'ReactJS is very simple'
+            },
+            {
+                name: 'AngularJS',
+                time: '50h',
+                free: true
+            },
+            {
+                name: 'NodeJS',
+                time: '60h',
+                free: true
+            }
+        ];
+        const elmCourses = items.map((item, index) =>
+            <Course key={index} name={item.name} time={item.time} free={item.free} desc="ReactJS is very simple"/>
+        );
         return (
             <div className="row">
-                <Course />
-                <Course />
-                <Course />
+                {elmCourses}
             </div>
         );
     }
--- my-app/src/component/Course.js
+++ my-app/src/component/Course.js
@@ -2,21 +2,63 @@ import React, { Component } from 'react';
 import Lesson from'./Lesson';
 
 class Course extends Component {
+    constructor(props) {
+        super(props);
+    
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick3 = this.handleClick3.bind(this);
+        this.handleClick4 = this.handleClick4.bind(this);
+    }
+
+    handleClick1() {
+        alert("View1");
+    }
+    handleClick2(content) {
+        alert(content);
+    }
+    handleClick3() {
+        alert(this.props.name);
+    }
+    handleClick4() {
+        alert(this.refs.username.value);
+    }
+
+    showButtonFree() {
+        const isFree = this.props.free;
+        console.log("isFree", isFree);
+        if(isFree === true) {
+            return <div className="panel-footer">
+                        <div className="panel-group">
+                            <button onClick={this.handleClick1} type="button" className="btn btn-success">View 1</button>
+                            <button onClick= {() => this.handleClick2("View 2")} type="button" className="btn btn-warning">View 2</button>
+                            <button onClick={this.handleClick3} type="button" className="btn btn-primary">View 3</button>
+                        </div>
+                        <div className="input-group">
+                            <span className="input-group-btn">
+                                <button className="btn btn-default" type="button" onClick={this.handleClick4}>Go!</button>
+                            </span>
+                            <input type="text" className="form-control" placeholder="username..." ref="username"/>
+                        </div>
+                    </div>;
+        }
+    }
     render() {
         return (
                 <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                     <div className="panel panel-info">
                         <div className="panel-heading">
-                            <h3 className="panel-title">ReactJS</h3>
+                            <h3 className="panel-title">{this.props.name}</h3>
                         </div>
                         <div className="panel-body">
+                            <p>{this.props.time}</p>
+                            <p>{this.props.children}</p>
                             <ul className="list-group">
                                 <Lesson />
                                 <Lesson />
                                 <Lesson />
                             </ul>
-                            <div><a class="btn btn-large btn-block btn-success" href="#a" role="button">Register</a></div>
                         </div>
+                        {this.showButtonFree()}
                     </div>
                 </div>
         );
